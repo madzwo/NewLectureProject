30a4ac9d5ae149bf83eb2784fa7bfd4162bc2365
madzwo <madzwo@iu.edu>
2024-03-19 13:58:06 -0400

    Bases shoot + rotate

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -11,29 +11,70 @@ public class Base : MonoBehaviour
 
     [SerializeField] GameObject flag;
 
+    [SerializeField] Transform turretTransform;
+    [SerializeField] Rigidbody2D turretRb;
+    public float rotationSpeed;
+
+    [SerializeField] Transform firePoint;
+    [SerializeField] GameObject bulletPrefab;
+    public float bulletSpeed;
+    public float fireRate;
+    private float timeTillFire;
+
 
 
     void Awake()
     {
         timeUntilSpawn = 0;
+        timeTillFire = fireRate;
+
     }
 
     void Update()
     {
-        timeUntilSpawn -= Time.deltaTime;
 
         if (timeUntilSpawn <= 0)
         {
             Instantiate(billion, spawnPoint.gameObject.transform.position, Quaternion.identity);
-            SetTimeUntilSpawn();
+            timeUntilSpawn = spawnRate;
+
         }
+        timeUntilSpawn -= Time.deltaTime;
+
+
+        RotateBarrel();
+        Fire();
+
         
     }
 
-    private void SetTimeUntilSpawn()
+    void RotateBarrel()
     {
-        timeUntilSpawn = spawnRate;
+        turretRb.rotation -= rotationSpeed;
     }
+
+    public void Fire()
+    {
+        if(GameObject.FindGameObjectWithTag("yellowBillion") != null)
+        {
+            GameObject targetBillion = GameObject.FindGameObjectWithTag("yellowBillion");
+            Vector3 direction = targetBillion.transform.position - firePoint.position;
+
+            if (timeTillFire <= 0)
+            {
+                GameObject bullet = Instantiate(bulletPrefab, firePoint.transform.position, firePoint.rotation);
+                bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+                timeTillFire = fireRate;
+            } 
+            else
+            {
+                timeTillFire -= Time.deltaTime;
+            }
+        }        
+        
+        
+    }
+
 
     // public void SpawnFlag() 
     // {
