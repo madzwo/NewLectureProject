ad28f10f38f4d2ab11fd0ef6c3175a22c2e66682
madzwo <madzwo@iu.edu>
2024-04-23 02:27:23 -0400

    drag lines
    why are they pink

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,6 +16,22 @@ public class GameController : MonoBehaviour
     public List<GameObject> blueFlags = new List<GameObject>();
     public List<GameObject> orangeFlags = new List<GameObject>();
 
+    public Material lineMaterial;
+    public LineRenderer lineRenderer;
+
+    Vector2 startPosition;
+
+
+    void Start()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = lineMaterial;
+        lineRenderer.startWidth = 0.02f;
+        lineRenderer.endWidth = 0.02f;
+        lineRenderer.positionCount = 2;
+    }
+
+
     void Update()
     {
         // makes mouse position work with camera    
@@ -90,34 +106,68 @@ public class GameController : MonoBehaviour
         {
             if(Input.GetMouseButton(0) && Vector2.Distance(flags[0].transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < .2)
             {
+                // Set startPosition only when the mouse button is pressed
+                if (Input.GetMouseButtonDown(0))
+                {
+                    startPosition = flags[0].transform.position;
+                    lineRenderer.enabled = true;
+
+                }                
                 flags[0].transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                // set Z position based on the camera's viewport
-                float desiredZ = Camera.main.ScreenToWorldPoint(Input.mousePosition).z + Camera.main.nearClipPlane;
-                Vector3 newPosition = flags[0].transform.position;
-                newPosition.z = desiredZ;
+                Vector2 newPosition = flags[0].transform.position;
                 flags[0].transform.position = newPosition;
+                //set Z position based on the camera's viewport
+                float desiredZ = Camera.main.ScreenToWorldPoint(Input.mousePosition).z + Camera.main.nearClipPlane;
+                flags[0].transform.position = new Vector3(flags[0].transform.position.x, flags[0].transform.position.y, desiredZ);
+                lineRenderer.SetPosition(0, startPosition);
+                lineRenderer.SetPosition(1, newPosition);
+                // Debug.DrawLine(startPosition, newPosition, Color.red);
             }
         }
         if(flags.Count == 2)
         {
             if(Input.GetMouseButton(0) && Vector2.Distance(flags[0].transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < .2)
             {
+                // Set startPosition only when the mouse button is pressed
+                if (Input.GetMouseButtonDown(0))
+                {
+                    startPosition = flags[0].transform.position;
+                    lineRenderer.enabled = true;
+                } 
                 flags[0].transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                // set Z position based on the camera's viewport
-                float desiredZ = Camera.main.ScreenToWorldPoint(Input.mousePosition).z + Camera.main.nearClipPlane;
-                Vector3 newPosition = flags[0].transform.position;
-                newPosition.z = desiredZ;
+                Vector2 newPosition = flags[0].transform.position;
                 flags[0].transform.position = newPosition;
+                //set Z position based on the camera's viewport
+                float desiredZ = Camera.main.ScreenToWorldPoint(Input.mousePosition).z + Camera.main.nearClipPlane;
+                flags[0].transform.position = new Vector3(flags[0].transform.position.x, flags[0].transform.position.y, desiredZ);
+                lineRenderer.SetPosition(0, startPosition);
+                lineRenderer.SetPosition(1, newPosition);
+                Debug.DrawLine(startPosition, newPosition, Color.red);
+
             }
             else if(Input.GetMouseButton(0) && Vector2.Distance(flags[1].transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) < .2)
             {
+                // Set startPosition only when the mouse button is pressed
+                if (Input.GetMouseButtonDown(0))
+                {
+                    startPosition = flags[1].transform.position;
+                    lineRenderer.enabled = true;
+                } 
                 flags[1].transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                // set Z position based on the camera's viewport
-                float desiredZ = Camera.main.ScreenToWorldPoint(Input.mousePosition).z + Camera.main.nearClipPlane;
-                Vector3 newPosition = flags[1].transform.position;
-                newPosition.z = desiredZ;
+                Vector2 newPosition = flags[1].transform.position;
                 flags[1].transform.position = newPosition;
+                //set Z position based on the camera's viewport
+                float desiredZ = Camera.main.ScreenToWorldPoint(Input.mousePosition).z + Camera.main.nearClipPlane;
+                flags[1].transform.position = new Vector3(flags[1].transform.position.x, flags[1].transform.position.y, desiredZ);
+                lineRenderer.SetPosition(0, startPosition);
+                lineRenderer.SetPosition(1, newPosition);
+                Debug.DrawLine(startPosition, newPosition, Color.red);
             }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            lineRenderer.enabled = false;
         }
     }
 }
